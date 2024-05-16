c553a043684da8df323dac803779c84e1fcea75c
kevbravo <kevbravo99@gmail.com>
2024-05-15 19:14:58 -0700

    Add files via upload

---
+++ crypto.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+lookup1 = "\n \"#()*+/1:=[]abcdefghijklmnopqrstuvwxyz"
+lookup2 = "ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst"
+chars = "DLSeGAGDgBNJDQJDCFSFnRBIDjgHoDFCFtHDgJpiHtGDmMAQFnRBJKkBAsTMrsPSDDnEFCFtIbEDtDCIbFCFtHTJDKerFldbFObFCFtLBFkBAAAPFnRBJGEkerFlcPgKkImHnIlATJDKbTbFOkdNnsgbnJRMFnRBNAFkBAAAbrcbTKAkOgFpOgFpOpkBAAAAAAAiClFGIPFnRBaKliCgClFGtIBAAAAAAAOgGEkImHnIl"
+chars_len = len(chars)
+out = ""
+prev = 0
+for char in chars:
+    cur = lookup2.index(char)
+    out += lookup1[(cur + prev) % 40]
+    prev = lookup1.index(lookup1[(cur + prev) % 40])
+
+print(out)
